42a5530ba39fbf20dd504495380c798d026036a1
ellieheo <sunheeheo10@gmail.com>
2021-10-30 18:28:07 +0900

    model 틀 추가

--- routes/detail.js
+++ routes/detail.js
@@ -1,9 +1,7 @@
 // 선희님! 하시죠! 
-
-// const express = require('express');
-// const router = express.Router();
-
-
+const express = require('express');
+const router = express.Router();
+const { db } = require('../models/index');
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
