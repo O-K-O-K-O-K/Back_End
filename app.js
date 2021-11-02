ffaadc8adfa9c4a2e0347e4e892e24f9122a3333
Eu Jeong Hwang <59908525+hj2bf@users.noreply.github.com>
2021-11-03 02:42:22 +0900

    eujeong add multer-s3 package

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const bodyParser =require('body-parser')
 const app = express();
@@ -12,18 +11,17 @@ const swaggerFile = require("./swagger-output");
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 require('dotenv').config();
 // app.use(compression());
 
-// const dogsRouter = require('./routes/dog');
+const dogsRouter = require('./routes/dog');
 // const detailRouter = require('./routes/detail');
 // const mainRouter = require('./routes/main');
 // const pagesRouter = require('./routes/mypage');
 // const usersRouter = require('./routes/user');
 
-// app.use('/dogs', dogsRouter);
+app.use('/dogs', dogsRouter);
 // app.use('/posts', detailRouter);
 // app.use('/posts', mainRouter);
 // app.use('/users', usersRouter);
@@ -43,10 +41,6 @@ app.use(cors(corsOptions));
 
 //swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 
 // catch 404 and forward to error handler
