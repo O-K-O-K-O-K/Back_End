61b965e1028a575b656686444db486880ac538d7
DESKTOP-acer <takgue1224@gmail.com>
2021-11-11 16:44:56 +0900

    [211111] feat morgan winston file fix

--- app.js
+++ app.js
@@ -1,55 +1,51 @@
-const createError = require('http-errors');
-const express = require('express');
+const createError = require("http-errors");
+const express = require("express");
 // const socketIo = require("socket.io")
-// const logger = require('morgan');
+// const authMiddleware = require("./middlewares/auth");
+const logger = require("morgan");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger-output");
-const path = require('path');
-// const authMiddleware = require("./middlewares/auth");
-// app.use(logger('dev'));
+// const path = require("path");
 
-const cors = require('cors');
+app.use(logger("dev"));
+
+const cors = require("cors");
 const corsOptions = {
-  origin: "*", 
+  origin: "*",
   // methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
   // preflightContinue: false,
   credentials: true,
   // optionsSuccessStatus: 204,
 };
-app.use(cors(corsOptions));
 
 //parser
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static('public'));
-
-require('dotenv').config();
+app.use(express.static("public"));
+require("dotenv").config();
 
 // app.use(compression());
 // app.set('views', path.join(__dirname, 'views'));
 
-const dogsRouter = require('./routes/dog');
-const detailRouter = require('./routes/detail');
-const infoRouter = require('./routes/userInfo');
-const usersRouter = require('./routes/user');
-const dogstaRouter = require('./routes/dogsta')
-const pagesRouter = require('./routes/mypage')
+const dogsRouter = require("./routes/dog");
+const detailRouter = require("./routes/detail");
+const infoRouter = require("./routes/userInfo");
+const usersRouter = require("./routes/user");
+const dogstaRouter = require("./routes/dogsta");
+const pagesRouter = require("./routes/mypage");
 
-app.use('/dogs', dogsRouter);
-app.use('/posts', detailRouter);
-app.use('/users', infoRouter);
-app.use('/users', usersRouter);
-app.use('/dogsta', dogstaRouter);
-app.use('/mypage', pagesRouter);
+app.use("/dogs", dogsRouter);
+app.use("/posts", detailRouter);
+app.use("/users", infoRouter);
+app.use("/users", usersRouter);
+app.use("/dogsta", dogstaRouter);
+app.use("/mypage", pagesRouter);
 
-app.get("/",(_,res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
-const auth = require('./middlewares/auth');
-
+// const auth = require("./middlewares/auth");
 
 //swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -67,6 +63,5 @@ app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 //   res.status(err.status || 500);
 //   res.render('error');
 // });
-
 
 module.exports = app;
