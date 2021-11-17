06a85f03b6ce011053422220688e56e0da404831
ellieheo <sunheeheo10@gmail.com>
2021-11-17 15:26:31 +0900

    feat: 로그 관리 모듈 등록 및 설정

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
+
+//모듈
 const createError = require("http-errors");
 const express = require("express");
-// const socketIo = require("socket.io")
-// const authMiddleware = require("./middlewares/auth");
-const morgan = require("morgan");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output");
 const path = require("path");
+const morgan = require("morgan");
 
-
+//cors
 const cors = require("cors");
 const corsOptions = {
   origin: "*",
   credentials: true,
 };
 
-//parser
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
-require("dotenv").config();
-app.use(morgan('dev'));
-
-// app.use(compression());
-// app.set('views', path.join(__dirname, 'views'));
-
+//라우팅
+const swaggerFile = require("./swagger-output");
 const dogsRouter = require("./routes/dog");
 const detailRouter = require("./routes/detail");
 const infoRouter = require("./routes/userInfo");
@@ -33,7 +23,20 @@ const usersRouter = require("./routes/user");
 const dogstaRouter = require("./routes/dogsta");
 const pagesRouter = require("./routes/mypage");
 const chatRouter = require("./routes/chat");
+const accessLogStream = require('./src/config/log');
 
+//앱세팅
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+require("dotenv").config();
+app.use(morgan('dev')); //정규님이 하신것
+app.use(morgan("common", {stream : accessLogStream })) //콘솔에 출력하기 위한 용도 //스트림은 데이터가 다니는 통로 //dev는 지정 가능 (Tokens) 
+// app.use(compression());
+// app.set('views', path.join(__dirname, 'views'));
+
+//미들웨어
 app.use("/dogs", dogsRouter);
 app.use("/posts", detailRouter);
 app.use("/users", infoRouter);
@@ -41,10 +44,10 @@ app.use("/users", usersRouter);
 app.use("/dogsta", dogstaRouter);
 app.use("/mypage", pagesRouter);
 app.use("/chat", chatRouter);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 
 app.get("/", (_, res) => res.render("home"));
 
-//swagger
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 module.exports = app;
