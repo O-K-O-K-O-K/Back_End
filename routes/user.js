0a3e50de3ad24cf01b3601788a4e3516f130d3ab
Eu Jeong Hwang <59908525+hj2bf@users.noreply.github.com>
2021-11-27 16:23:13 +0900

    [211127] feat: controllers 초기 세팅

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const setRounds = 10;
 const router = express.Router();
 const util = require("util");
-const passport = require('passport');
-const KakaoStrategy = require('passport-kakao').Strategy;
 const { db } = require("../models/index");
 const dotenv = require("dotenv");
 dotenv.config();
 db.query = util.promisify(db.query);
-const upload = require("../S3/s3");  // 여기
-
-
+const upload = require("../S3/s3"); // 여기
 
 //로그인
 router.post("/login", async (req, res) => {
@@ -36,118 +32,87 @@ router.post("/login", async (req, res) => {
           image: users.userImage,
         },
         process.env.SECRET_KEY,
-        { expiresIn: "144h" } 
+        { expiresIn: "144h" }
       );
       // res.cookie('user', token);  쿠키!
       res.json({ token, user: users.userId });
-      console.log("유저아이디:",users.userId)
+      console.log("유저아이디:", users.userId);
     } else {
-      res.status(400).send({result: "비밀번호를 확인해주세요." });
+      res.status(400).send({ result: "비밀번호를 확인해주세요." });
     }
   } else {
     // 유저가 없다면? (가입하지 않았다면)
-    res.status(400).send({result: "이메일을 확인해주세요." });
+    res.status(400).send({ result: "이메일을 확인해주세요." });
   }
 });
-
-// // 로그아웃 211124
-// router.post("/logout") , async (req, res) => {
-//   const post =`SELECT * FROM user WHERE userEmail =?`;
-//   const results = await db.query(post, [userEmail]);
-//   users.userId
-// }
 
 //회원가입  여기 미들웨어(upload.single("userImage)
 router.post("/signUp", upload.single("userImage"), async (req, res) => {
-  const { userEmail, password, confirmPassword, userNickname, userGender, userAge,userLocation} = req.body;
-  const userImage = req.file.location;   //여기 따로 지정
-  if (!await nicknameExist(userNickname)) {
-    // 닉네임 중복 검사
-    res.status(401).send({ result: "닉네임이 존재합니다." });
-  } else if (!idCheck(userEmail)) {
-    // id 정규식 검사
-    res.status(401).send({ result: "이메일 형식이 맞지 않습니다."});
-  } else if (!pwConfirm(password, confirmPassword)) {
-    // 비밀번호와 비밀번호 확인이 맞는지 검사
-    res.sendStatus(401);
-  } else if (!pwLenCheck(password)) {
-    // 비밀번호 최소길이 검사
-    res.sendStatus(401);
-  } else if (!pw_idCheck(userEmail, password)) {
-    // 아이디가 비밀번호를 포함하는지 검사
-    res.status(401).send({ result: "비밀번호 형식이 올바르지않습니다." });
-  } else {
-    const salt = await bcrypt.genSaltSync(setRounds);
-    const hashPassword = bcrypt.hashSync(password, salt);
-    const userParams = [userEmail, hashPassword, userNickname, userGender, userAge, userImage,userLocation];
-    const post =
-      "INSERT INTO user (userEmail, password, userNickname, userGender, userAge, userImage, userLocation) VALUES (?, ?, ? , ?, ?, ?, ?);";
-    db.query(post, userParams, (error, results, fields) => {
-      // db.query(쿼리문, 넣을 값, 콜백)
-      if (error) {
-        res.status(401).send(error);
-        console.log(error);
-      } else {
-        console.log("누군가가 회원가입을 했습니다.");
-        res.send({ results: "완료" });
-      }
-    });
-  }
+  const {
+    userEmail,
+    password,
+    confirmPassword,
+    userNickname,
+    userGender,
+    userAge,
+    userLocation,
+  } = req.body;
+  console.log(
+    "회원가입",
+    userEmail,
+    password,
+    confirmPassword,
+    userNickname,
+    userGender,
+    userAge,
+    userLocation
+  );
+
+  const userImage = req.file.location; //여기 따로 지정
+
+  const salt = await bcrypt.genSaltSync(setRounds);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  const userParams = [
+    userEmail,
+    hashPassword,
+    userNickname,
+    userGender,
+    userAge,
+    userImage,
+    userLocation,
+  ];
+  const post =
+    "INSERT INTO user (userEmail, password, userNickname, userGender, userAge, userImage, userLocation) VALUES (?, ?, ? , ?, ?, ?, ?);";
+  db.query(post, userParams, (error, results, fields) => {
+    // db.query(쿼리문, 넣을 값, 콜백)
+    if (error) {
+      console.log("저장", error);
+      res.status(401).send(error);
+      console.log(error);
+    } else {
+      console.log("누군가가 회원가입을 했습니다.");
+      res.send({ results: "완료" });
+    }
+  });
 });
 
-//이메일 중복확인 
+//이메일 중복확인
 router.post("/checkDup", async (req, res) => {
   const { userEmail } = req.body;
- if (idCheck(userEmail)) {
-  if (!await emailExist(userEmail)) {
-    res.status(401).send({ result: "이메일이 존재합니다." });
-  } else {
-    res.status(200).send({ result: "정상적인 이메일입니다."})
-  }} else {
-    res.status(400).send({ result: "이메일 형식이 맞지않습니다."})
+  if (idCheck(userEmail)) {
+    if (!(await emailExist(userEmail))) {
+      res.status(401).send({ result: "이메일이 존재합니다." });
+    } else {
+      res.status(200).send({ result: "정상적인 이메일입니다." });
+    }
   }
 });
-
-// //id 정규식 처리
-// function idCheck(idGive) {
-//   console.log(idGive);
-//   const reg_name = /^[a-z]+[a-z0-9]{5,19}$/g;
-//   if (reg_name.test(idGive) && idGive.length >= 6) {
-//     return true;
-//   }
-//   return false;
-// }
 
 //email 정규식 처리
 function idCheck(idGive) {
   const reg_name =
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-  if (reg_name.test(idGive) && idGive.split('@')[0].length >= 6) {
-    return true;
-  }
-  return false;
-}
-
-
-
-function pwConfirm(pwGive, pw2Give) {
-  console.log(pwGive,pw2Give);
-  if (pwGive === pw2Give) {
-    return true;
-  }
-  return false;
-}
-
-function pwLenCheck(pwGive) {
-  console.log(pwGive);
-  if (pwGive.length >= 4) {
-    return true;
-  }
-  return false;
-}
-
-function pw_idCheck(idGive, pwGive) {
-  if (!idGive.includes(pwGive)) {
+  if (reg_name.test(idGive) && idGive.split("@")[0].length >= 3) {
     return true;
   }
   return false;
@@ -161,7 +126,7 @@ function emailExist(userEmail) {
       console.log(results);
       if (error) {
         // logger.error(`Msg: raise Error in checkValidationEmail => ${error}`);
-        console.log(error)
+        console.log(error);
         return resolve(false);
       }
 
@@ -174,30 +139,6 @@ function emailExist(userEmail) {
       resolve(false);
     });
   });
-}
-
-async function nicknameExist(nickGive) {
-  const post = `SELECT * FROM user WHERE userNickname = ?;`;
-  const results = await db.query(post, [nickGive]);
-  if (results.length) {
-    // Boolean([])  true이다.
-    return false;
-  } else {
-    return true;
-  }
-}
-
-
-//카카오 로그인
-module.exports = (passport) => {
-  passport.use('kakao', new KakaoStrategy({
-    clientID: '856ec0be1a62b01007353103f2cbc64d',
-    callbackURL: '/auth/login',
-  }, async (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
-    console.log(accessToken);
-    console.log(refreshToken);
-  }))
 }
 
 
