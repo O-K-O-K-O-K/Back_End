76fdc1fff73b1452b7351abf30aa20dbbbc749f5
DESKTOP-tak <takgue1224@gmail.com>
2021-12-17 17:53:00 +0900

    confirm

--- app.js
+++ app.js
@@ -72,7 +72,10 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ 
+    message: err.message,
+    error: err
+  });
 });
 
 
