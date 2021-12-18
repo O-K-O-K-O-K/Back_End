1477656d4456874d81380fbeeb88de3400931995
DESKTOP-tak <takgue1224@gmail.com>
2021-12-18 17:20:23 +0900

    [211218] chore : root 오타체크

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,10 @@ dotenv.config();
 const ctrl = require("../controllers/user/user.ctrl");
 
 //로그인
-// 강아지 정보 등록하기
 router.post("/login", ctrl.loginUser);
-// 회원가입  여기 미들웨어(upload.single("userImage)
-router.post("/singUp", upload.single("userImage"), ctrl.postUser);
+// 회원가입 
+router.post("/signUp", upload.single("userImage"), ctrl.postUser);
 //이메일 중복확인]
 router.post("/checkDup", ctrl.checkDup);
-
 
 module.exports = router;
